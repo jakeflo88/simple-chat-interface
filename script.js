2f93d7a06f631fb6253fab48ad8ed6c57d753f05
jakeflo88 <jakeflorian.mcf@gmail.com>
2014-10-03 16:09:24 -0300

    Presentation commit retry

--- script.js
+++ script.js
@@ -1,7 +1,40 @@
-function sendMessage() {
-	console.log("This first one");
-	var text = document.getElementById("userMessage");
-	console.log("They said: " + text);
-}
+//default name
+var nick = "Anonymous";
 
-console.log("This works");
+//function for posting messages
+function addMessage() {
+	
+	//get the message and clear the textarea
+	var text = document.getElementById("userMessage").value;
+	document.getElementById("userMessage").value = "";
+	document.getElementById("userMessage").focus();
+
+	//check for nick command
+	if (text.indexOf("/nick") > -1){
+		nick = text.substring(6);
+		text = ("Name changed to: '" + nick + "'");
+
+		var post = document.createElement("P");
+		var content = document.createTextNode(text);
+		post.appendChild(content);
+		document.getElementById("messages").appendChild(post);
+		return;
+	}
+	
+	//if nothing is typed
+	if (text === "") {
+		//do nothing
+	}
+
+	//for regular messages
+	else {
+		var post2 = document.createElement("P");
+		var content2 = document.createTextNode(nick + " says: " + text);
+		post2.appendChild(content2);
+		document.getElementById("messages").appendChild(post2);
+	}
+
+	//for autoscroll to the bottom
+	var messageLog = document.getElementById("messages");
+	messageLog.scrollTop = messageLog.scrollHeight;
+}
