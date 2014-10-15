ec19bc55fe3db124b6b1ab1d255ee272edaa6f7b
jakeflo88 <jakeflorian.mcf@gmail.com>
2014-10-15 11:22:19 -0300

    Node modules and name generator added

--- script.js
+++ script.js
@@ -1,68 +1,149 @@
-//default name
-var nick = "Anonymous";
+//default name generator
+var guest = Math.random();
+var guestName = guest.toString();
+var guestNumber = guestName.substring(2, 6);
+
+var nick = ("Guest" + guestNumber);
+var post = [nick];
+
+//socket emit new user connection
+socket.emit('signin', nick);
 
 
 //to make the enter button work
 document.getElementById("userMessage").addEventListener('keyup',function(event){
 	if (event.keyCode === 13){
+		//prevent null chat messages
+		var preventNull = document.getElementById('userMessage').value;
+		preventNull = preventNull.substring(0, preventNull.length - 1);
+		document.getElementById('userMessage').value = preventNull;
+
 		//run the msg function
 		addMessage();
 	}
 });
-
-
 
 //function for posting messages
 function addMessage() {
 	
 	//get the message and clear the textarea
 	var text = document.getElementById("userMessage").value;
-	document.getElementById("userMessage").value = null;
+
+	//making an array to seperate name from message
+	post.push(nick, text);
+
+	//reset and focus on textarea
+	document.getElementById("userMessage").value = "";
 	document.getElementById("userMessage").focus();
+
 
 	//check for nick command
 	if (text.indexOf("/nick") > -1){
+		//removing the /nick part of the message
 		nick = text.substring(6);
-		text = ("Name changed to: '" + nick + "'");
 
+		if (nick.match(/[a-z]/i)) {
 
-		var nameChangeInsert = document.createElement("DIV");
+		//text for the name change post
+		text = (post[0] + " is now known as: '" + nick + "'");
 
-		var nameChange = document.createElement("DIV");
-		nameChange.appendChild(document.createTextNode(text));
-		nameChange.setAttribute("id", "changeId");
+		//updating array
+		post[1] = nick;
+		post[2] = text;
 
-		nameChangeInsert.appendChild(nameChange);
+		socket.emit('chat message', post);
+		}
 
-		document.getElementById("messages").appendChild(nameChangeInsert);
-		
+	//prevent empty username
+	else {
+		alert("Please enter a valid username.");
+	}
 		return;
 	}
 	
 	//if nothing is typed
 	if (text === "") {
 		//do nothing
+		return;
 	}
 
 	//for regular messages
 	else {
-		var newPost = document.createElement("DIV");
+		//updating array
+		post[0] = nick;
+		post[1] = nick;
+		post[2] = text;
 
-		var namePost = document.createElement("DIV");
-		namePost.appendChild(document.createTextNode(nick + " says:"));
-		namePost.setAttribute("id", "nameId");
-
-		var chatPost = document.createElement("P");
-		chatPost.appendChild(document.createTextNode(text));
-		chatPost.setAttribute("id", "chatId");
-
-		newPost.appendChild(namePost);
-		newPost.appendChild(chatPost);
-
-		document.getElementById("messages").appendChild(newPost);
+		//taking the new post and sending to server
+		socket.emit('chat message', post);
 	}
-
-	//for autoscroll to the bottom
-	var messageLog = document.getElementById("messages");
-	messageLog.scrollTop = messageLog.scrollHeight;
 }
+
+	//listening for messages
+	socket.on('chat message', function(post){
+		//adding the messages to the page
+
+		//for chat posts
+		if(post[0] === post[1]){
+			//creating an element to post
+			var newPost = document.createElement("DIV");
+
+			//creating the name element and giving ID
+			var namePost = document.createElement("DIV");
+			namePost.appendChild(document.createTextNode(post[1] + " says:"));
+			namePost.setAttribute("id", "nameId");
+
+			//creating the chat element and giving ID
+			var chatPost = document.createElement("P");
+			chatPost.appendChild(document.createTextNode(post[2]));
+			chatPost.setAttribute("id", "chatId");
+
+			//appending both to the post element
+			newPost.appendChild(namePost);
+			newPost.appendChild(chatPost);
+	
+			//appending the post to the message area
+			document.getElementById("messages").appendChild(newPost);
+
+			//for autoscroll to the bottom
+			var messageLog2 = document.getElementById("messages");
+			messageLog2.scrollTop = messageLog2.scrollHeight;
+
+			return;
+		}
+
+		//creating element for name change posts
+		var nameChangeInsert = document.createElement("DIV");
+
+		//creating element for the text node and giving ID
+		var nameChange = document.createElement("DIV");
+		nameChange.appendChild(document.createTextNode(post[2]));
+		nameChange.setAttribute("id", "changeId");
+
+		//appending the name change to the name change post
+		nameChangeInsert.appendChild(nameChange);
+
+		//appending the name change post to the message area
+		document.getElementById("messages").appendChild(nameChangeInsert);
+
+		//for autoscroll to the bottom
+		var messageLog3 = document.getElementById("messages");
+		messageLog3.scrollTop = messageLog3.scrollHeight;
+});
+
+socket.on('signedin', function(nick){
+
+	//for new users entering chat
+	var newUserConnect = document.createElement("DIV");
+
+	var newUser = document.createElement("DIV");
+	newUser.appendChild(document.createTextNode(nick + " has joined the room."));
+	newUser.setAttribute("id", "changeId");
+
+	newUserConnect.appendChild(newUser);
+
+	document.getElementById("messages").appendChild(newUserConnect);
+
+	var messageLog1 = document.getElementById("messages");
+	messageLog1.scrollTop = messageLog1.scrollHeight;
+});
---
+++ server.js
@@ -0,0 +1,30 @@
+//server functionality
+var express = require('express');
+
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+	//for chat messages
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+
+  //for signins
+  socket.on('signin', function(nick){
+  	console.log(nick + "is here");
+  	io.emit('signedin', nick);
+  });
+
+});
+
+app.use(express.static(__dirname));
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
